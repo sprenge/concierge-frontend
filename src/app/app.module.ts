f6bd0cdbd3bdaeb285bfd44ccdb5e5b8e4323d26
Erwin Sprengers <erwin.sprengers@nokia.com>
2021-01-14 08:09:09 +0100

    Draft version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { UnknownObjectCardComponent } from './unknown-object-card/unknown-object-card.component';
+import { UnknownObjectListComponent } from './unknown-object-list/unknown-object-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    UnknownObjectCardComponent,
+    UnknownObjectListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
